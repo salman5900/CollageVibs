49ea0c03602423128966bccf3352135712b3cbf6
Salman Ahamed <salmanahamed5900@gmail.com>
2025-05-20 18:40:58 +0530

    basic setup done for global chat

--- globalchat/views.py
+++ globalchat/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import *
 
 # Create your views here.
+@login_required
 def global_chat(request):
-    return render(request, 'globalchat/global_chat.html')
+    global_chat_messages = GlobalChatMessage.objects.all()[:30]
+    return render(request, 'globalchat/global_chat.html', {'global_chat_messages': global_chat_messages})
