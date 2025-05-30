27850cac1ceb12181fc91886aa1aff18c206d28f
Salman Ahamed <salmanahamed5900@gmail.com>
2025-05-30 17:14:23 +0530

    navbar scroll background

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,9 @@
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.navbar');
 
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
