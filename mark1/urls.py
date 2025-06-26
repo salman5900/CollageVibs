8a72d6817efc516e6959d5f8fb71ff1651bfe450
Salman Ahamed <salmanahamed5900@gmail.com>
2025-06-26 17:01:52 +0000

    PHASE 10

--- mark1/urls.py
+++ mark1/urls.py
@@ -28,6 +28,5 @@ urlpatterns = [
     path('noticeboard/', include('noticeboard.urls')),
 
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
