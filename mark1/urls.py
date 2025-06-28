c97d21b4bfdbb790140e949e1790566a574ea4b3
Salman Ahamed <salmanahamed5900@gmail.com>
2025-06-28 15:25:20 +0000

    testcase 3

--- mark1/urls.py
+++ mark1/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin,re_path
-from django.views.static import serve
+from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -29,9 +28,5 @@ urlpatterns = [
     path('noticeboard/', include('noticeboard.urls')),
 
 ]
-# Only for dev mode (if DEBUG=True)
-if settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-    ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
